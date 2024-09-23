1e9c5f073f885787548ce877ccb39cb51b66bfc2
code_st <1035802659@qq.com>
2024-09-23 19:53:48 +0800

    Update: product kernel 扩展到 f16 f64 int8

--- galois/op/matrix_multiply_op.hpp
+++ galois/op/matrix_multiply_op.hpp
@@ -2,6 +2,7 @@
 
 #include "galois/graph/graph.hpp"
 #include "galois/ir/ir.hpp"
+#include <iostream>
 
 namespace galois::op {
 
@@ -13,10 +14,25 @@ class ProductKernel : public Kernel {
                std::vector<std::shared_ptr<Tensor>> ir_outputs, std::shared_ptr<Builder>) override {
         auto ir_mat_a = ir_inputs[0];
         auto ir_mat_b = ir_inputs[1];
-        if ((ir_mat_a->type == TensorType::CreateMatrixType(FloatType::Create(32), 4, 1)) &&
-            ir_mat_b->type == TensorType::CreateMatrixType(FloatType::Create(32), 1, 4)) {
-            return true;
+        
+        auto supported_types = {
+            TensorType::CreateMatrixType(FloatType::Create(16), 4, 1), //float16
+            TensorType::CreateMatrixType(FloatType::Create(32), 4, 1), //float32
+            TensorType::CreateMatrixType(FloatType::Create(64), 4, 1), //float64
+            TensorType::CreateMatrixType(IntType::Create(8, true), 4, 1) // int8
+        
+        };
+
+        for (const auto& type : supported_types) {
+            if (ir_mat_a->type == type && ir_mat_b->type == TensorType::CreateMatrixType(type->value_type, 1, 4)) {
+                return true;
+            }
         }
+
+        // if ((ir_mat_a->type == TensorType::CreateMatrixType(FloatType::Create(32), 4, 1)) &&
+        //     ir_mat_b->type == TensorType::CreateMatrixType(FloatType::Create(32), 1, 4)) {
+        //     return true;
+        // }
         return false;
     }
 
@@ -28,11 +44,34 @@ class ProductKernel : public Kernel {
         auto ir_mat_c = ir_outputs[0];
         Eigen::VectorXi64 v4(1);
         v4[0] = 4;
-        auto ir_f32x4_type = TensorType::Create(FloatType::Create(32), v4);
-        auto ir_bit_cast_a = ir_builder->Create<BitCast>(ir_mat_a, ir_f32x4_type);
-        auto ir_bit_cast_b = ir_builder->Create<BitCast>(ir_mat_b, ir_f32x4_type);
-        auto ir_bit_cast_c =
-            ir_builder->Create<BitCast>(ir_mat_c, TensorType::Create(ir_f32x4_type, v4));
+
+        // auto ir_f32x4_type = TensorType::Create(FloatType::Create(32), v4);
+        // auto ir_bit_cast_a = ir_builder->Create<BitCast>(ir_mat_a, ir_f32x4_type);
+        // auto ir_bit_cast_b = ir_builder->Create<BitCast>(ir_mat_b, ir_f32x4_type);
+        // auto ir_bit_cast_c =
+        //     ir_builder->Create<BitCast>(ir_mat_c, TensorType::Create(ir_f32x4_type, v4));
+
+        // GALOIS_ASSERT(Cast<FloatType>(ir_mat_a->type->data_type));
+        std::shared_ptr<TensorType>  ir_f_type;
+        if (auto ir_float_type = Cast<FloatType>(ir_mat_a->type->data_type)) {
+            if(ir_float_type->bits == 16) {
+                ir_f_type = TensorType::Create(FloatType::Create(16), v4); 
+            } else if (ir_float_type->bits == 32) {
+                ir_f_type = TensorType::Create(FloatType::Create(32), v4);
+            } else if (ir_float_type->bits == 64) {
+                ir_f_type = TensorType::Create(FloatType::Create(64), v4);
+            }
+        } 
+        else if (auto ir_int_type = Cast<IntType>(ir_mat_a->type->data_type)) {
+            if (ir_int_type->bits == 8) {
+                ir_f_type = TensorType::Create(IntType::Create(8, true), v4);
+            }
+        }
+    
+        // 把输入tensor 转化为指定的类型
+        auto ir_bit_cast_a = ir_builder->Create<BitCast>(ir_mat_a, ir_f_type);
+        auto ir_bit_cast_b = ir_builder->Create<BitCast>(ir_mat_b, ir_f_type);
+        auto ir_bit_cast_c = ir_builder->Create<BitCast>(ir_mat_c, TensorType::Create(ir_f_type,v4));
 
         for (int64_t i = 0; i < 4; ++i) {
             auto ir_vector_broadcast_a = ir_builder->Create<VectorBroadcast>(ir_bit_cast_a, i);
